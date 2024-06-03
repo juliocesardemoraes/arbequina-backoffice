6eb92e256b2721c91d4f124e0bc8b616a67e756c
julio cesar <jcmcf2012@gmail.com>
2024-06-03 13:54:27 -0300

    feat: add sidebar

---
+++ src/app/dash/page.tsx
@@ -0,0 +1,11 @@
+import { Dashboard } from "@/components/fullwindow/fullwindow";
+import { Sidebar } from "@/components/sidebar/Sidebar";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <Dashboard></Dashboard>
+    </>
+  );
+}
