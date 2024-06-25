a8a61407284ad8a1b908840255b14c877072468c
DDR23 <roboreserva.andre@gmail.com>
2024-06-25 20:03:27 -0300

    feat:{adicionando modais de edição de produtos}

--- src/components/pages/produto/produtoId/PageProdutoId.tsx
+++ src/components/pages/produto/produtoId/PageProdutoId.tsx
@@ -1,4 +1,8 @@
+import DialogEditProductCategory from "@/components/dialogsProduct/DialogEditProductCategory"
 import DialogEditProductName from "@/components/dialogsProduct/DialogEditProductName"
+import DialogEditProductPrice from "@/components/dialogsProduct/DialogEditProductPrice"
+import DialogEditProductQuantity from "@/components/dialogsProduct/DialogEditProductQuantity"
+import DialogEditProductStatus from "@/components/dialogsProduct/DialogEditProductStatus"
 import { Card, CardContent } from "@/components/ui/card"
 import { Dialog, DialogTrigger } from "@/components/ui/dialog"
 import { Pen } from "lucide-react"
@@ -39,28 +43,56 @@ export default function PageProdutoId({ produto, categorias }: any) {
             <div className="mb-5 pb-2 border-b">
               <div className="w-full justify-between text-sm flex items-center gap-2 text-muted-foreground md:inline-flex">
                 Categoria
-                <Pen size={20} />
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Pen style={{
+                      cursor: 'pointer'
+                    }} size={20} />
+                  </DialogTrigger>
+                  <DialogEditProductCategory productId={productId} authToken={authToken} category={categorias} />
+                </Dialog>
               </div>
               <div className="font-medium">{produto.PRODUCT_CATEGORY}</div>
             </div>
             <div className="mb-5 pb-2 border-b">
               <div className="w-full justify-between text-sm flex items-center gap-2 text-muted-foreground md:inline-flex">
                 Preço
-                <Pen size={20} />
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Pen style={{
+                      cursor: 'pointer'
+                    }} size={20} />
+                  </DialogTrigger>
+                  <DialogEditProductPrice productId={productId} authToken={authToken} />
+                </Dialog>
               </div>
               <div className="font-medium">{produto.PRODUCT_PRICE}</div>
             </div>
             <div className="mb-5 pb-2 border-b">
               <div className="w-full justify-between text-sm flex items-center gap-2 text-muted-foreground md:inline-flex">
                 Estoque
-                <Pen size={20} />
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Pen style={{
+                      cursor: 'pointer'
+                    }} size={20} />
+                  </DialogTrigger>
+                  <DialogEditProductQuantity productId={productId} authToken={authToken} />
+                </Dialog>
               </div>
               <div className="font-medium">{produto.PRODUCT_QUANTITY}</div>
             </div>
             <div className="mb-5 pb-2 border-b">
               <div className="w-full justify-between text-sm flex items-center gap-2 text-muted-foreground md:inline-flex">
                 Status
-                <Pen size={20} />
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Pen style={{
+                      cursor: 'pointer'
+                    }} size={20} />
+                  </DialogTrigger>
+                  <DialogEditProductStatus productId={productId} authToken={authToken} />
+                </Dialog>
               </div>
               <div className="font-medium">{produto.PRODUCT_DELETED ? 'Indisponível' : 'Disponível'}</div>
             </div>
